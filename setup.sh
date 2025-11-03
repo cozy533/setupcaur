7294f24657c3008b41b90f7e3bbce673ac3a0ee8
cozy533 <31392495+cozy533@users.noreply.github.com>
2025-11-03 18:54:46 -0500

    add comment

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 echo "Setting up Chaotic AUR..."
 
+# Taken directly from https://aur.chaotic.cx/docs
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
 pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
