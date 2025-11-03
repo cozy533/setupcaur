8b6231790b754a9b357bb28de46d7bca5fcf0c1a
cozy533 <31392495+cozy533@users.noreply.github.com>
2025-11-03 18:52:58 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+echo "Setting up Chaotic AUR..."
+
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+
+echo -e "\033[0;33mAll done.\033[0;0m"
